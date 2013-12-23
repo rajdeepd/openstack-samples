e8c2190634501722eb73e28f1a7eb7d5307b528f
user1 <user1@openstack1.(none)>
2013-12-23 10:27:22 +0530

    Renamed nova_list_servers file, modified nova_create_server

--- lab1/credentials.py
+++ lab1/credentials.py
@@ -14,3 +14,12 @@ def get_nova_credentials():
     d['auth_url'] = os.environ['OS_AUTH_URL']
     d['project_id'] = os.environ['OS_TENANT_NAME']
     return d
+
+def get_nova_credentials_v2():
+    d = {}
+    d['version'] = '2'
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
+    return d
