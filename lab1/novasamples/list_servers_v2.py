4f9e46c09f3e0ee35b5585d865bc42f84f07d43f
Rajdeep Dua <dua_rajdeep@yahoo.com>
2013-12-23 15:06:52 +0530

    Updated README.md

--- lab1/novasamples/list_servers_v2.py
+++ lab1/novasamples/list_servers_v2.py
@@ -1,6 +1,6 @@
 from credentials import get_nova_credentials_v2
 from novaclient.client import Client
-#nova = Client(2, "admin", "admin_pass", "admin", "http://192.168.101.15:35357/v2.0")
+
 credentials = get_nova_credentials_v2()
 nova_client = Client(**credentials)
 
