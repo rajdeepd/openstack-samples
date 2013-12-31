d8c6fb605fb3b2d9296f266dbf62705b40322404
rajdeep <dua_rajdeep@yahoo.com>
2013-12-31 16:01:18 +0530

    Added samples for list_ports for a server Create a port connecting a server to a network

--- lab1/utils.py
+++ lab1/utils.py
@@ -11,3 +11,21 @@ def print_values(val,type):
         for k,v in p.items():
     	    print "%s : %s" % (k, v)
         print '\n'    
+
+def print_values_server(val,server_id,type):
+    val_type = None
+    if type=='ports':
+         val_list = val['ports']
+    
+    if type=='networks':
+         val_list = val['networks']
+    #port_list = ports['ports']
+    for p in val_list:
+        bool = False
+        for k,v in p.items():
+            if k=='device_id' and v==server_id:
+		bool = True
+        if bool:
+            for k,v in p.items():
+    	        print "%s : %s" % (k, v)
+            print '\n'    
