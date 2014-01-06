403814217e703ea271768cf6519118ca7caec391
rajdeep <dua_rajdeep@yahoo.com>
2014-01-06 22:27:19 +0530

    Made changes to create_tenant.py

--- lab1/keystonesamples/create_tenant.py
+++ lab1/keystonesamples/create_tenant.py
@@ -6,23 +6,33 @@ try:
     keystone = ksclient.Client(**credentials)
     print keystone.auth_url
     #Looks like a potential bug
-    if keystone.auth_url != keystone.management_url:
-        keystone.management_url = keystone.auth_url
+    #if keystone.auth_url != keystone.management_url:
+    #    keystone.management_url = keystone.auth_url
+    tenant_name = 'user3-project'
+    tenant_exists = False
 
-    req_body = {
+    tenants_list = keystone.tenants.list()
+    for t in tenants_list:
+        if t.name ==  tenant_name:
+	    tenant_exists = True
+    if tenant_exists :
+        print 'Tenant %s exists.' % tenant_name
+    else:
+        print 'Creating tenant %s ' % tenant_name
+        req_body = {
             "tenant": {
-                "name": "tenantX",
-                "description": "Like tenant 9, but better.",
+                "name": tenant_name,
+                "description": "Tenant  user3-project",
                 "enabled": True,
                 "extravalue01": "metadata01",
             },
-    }
-    tenant = keystone.tenants.create(
+        }
+        tenant = keystone.tenants.create(
             req_body['tenant']['name'],
             req_body['tenant']['description'],
             req_body['tenant']['enabled']
             )
-    print tenant
+        print tenant
 
 finally:
     print "Execution Completed."
