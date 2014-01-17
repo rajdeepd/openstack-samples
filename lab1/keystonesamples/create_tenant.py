0c65735a6590840204e65a7ca505233048d0234c
akrantha2 <akrantha2@storage.akrantha>
2014-01-17 18:03:26 +0530

    fixed pep8 issues

--- lab1/keystonesamples/create_tenant.py
+++ lab1/keystonesamples/create_tenant.py
@@ -13,9 +13,9 @@ try:
 
     tenants_list = keystone.tenants.list()
     for t in tenants_list:
-        if t.name ==  tenant_name:
-	    tenant_exists = True
-    if tenant_exists :
+        if t.name == tenant_name:
+            tenant_exists = True
+    if tenant_exists:
         print 'Tenant %s exists.' % tenant_name
     else:
         print 'Creating tenant %s ' % tenant_name
@@ -30,8 +30,7 @@ try:
         tenant = keystone.tenants.create(
             req_body['tenant']['name'],
             req_body['tenant']['description'],
-            req_body['tenant']['enabled']
-            )
+            req_body['tenant']['enabled'])
         print tenant
 
 finally:
