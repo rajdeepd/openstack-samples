bd9be66b45c86502c366b3e965fe014e3b55d448
akrantha2 <akrantha2@storage.akrantha>
2014-01-18 18:47:31 +0530

    fixed pep8

--- lab1/neutronsamples/create_port.py
+++ lab1/neutronsamples/create_port.py
@@ -9,22 +9,22 @@ nova_client = nvclient.Client(**credentials)
 
 #replace with server_id and network_id from your environment
 
-server_id='9a52795a-a70d-49a8-a5d0-5b38d78bd12d'
-network_id= 'ce5d204a-93f5-43ef-bd89-3ab99ad09a9a'
+server_id = '9a52795a-a70d-49a8-a5d0-5b38d78bd12d'
+network_id = 'ce5d204a-93f5-43ef-bd89-3ab99ad09a9a'
 server_detail = nova_client.servers.get(server_id)
 print server_detail.id
 
-if server_detail != None:
+if server_detail is not None:
     credentials = get_credentials()
     neutron = client.Client(**credentials)
 
     body_value = {
-                     "port": {
-                             "admin_state_up": True,
-                             "device_id": server_id,
-                             "name": "port1",
-                             "network_id": network_id 
-                      }
-                 }
+        "port": {
+        "admin_state_up": True,
+        "device_id": server_id,
+        "name": "port1",
+        "network_id": network_id
+        }
+    }
     response = neutron.create_port(body=body_value)
     print response
--- lab1/neutronsamples/list_ports_server.py
+++ lab1/neutronsamples/list_ports_server.py
@@ -7,27 +7,27 @@ from credentials import get_nova_credentials
 credentials = get_nova_credentials()
 nova_client = nvclient.Client(**credentials)
 
-server_id='9a52795a-a70d-49a8-a5d0-5b38d78bd12d'
-network_id= 'ce5d204a-93f5-43ef-bd89-3ab99ad09a9a'
+server_id = '9a52795a-a70d-49a8-a5d0-5b38d78bd12d'
+network_id = 'ce5d204a-93f5-43ef-bd89-3ab99ad09a9a'
 server_detail = nova_client.servers.get(server_id)
 print server_detail.id
 #server_list = servers['servers']
 
 #for s in server_list:
 #    print s['id']
-if server_detail != None:
+if server_detail is not None:
     credentials = get_credentials()
     neutron = client.Client(**credentials)
     ports = neutron.list_ports()
 
-    print print_values_server(ports,server_id, 'ports')
+    print print_values_server(ports, server_id, 'ports')
     body_value = {
-                     "port": {
-                             "admin_state_up": True,
-                             "device_id": server_id,
-                             "name": "port1",
-                             "network_id": network_id 
-                      }
-                 }
+        "port": {
+        "admin_state_up": True,
+        "device_id": server_id,
+        "name": "port1",
+        "network_id": network_id
+        }
+    }
     response = neutron.create_port(body=body_value)
     print response
