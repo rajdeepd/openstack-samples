3da50f7f0fb0cf4dccdbeba414ca1c959252fd3b
havana <avenger@havana.(none)>
2014-01-23 15:39:26 +0530

    "creating volume in v1 also checked with pep8"

---
+++ lab1/cindersamples/create_volume_v1.py
@@ -0,0 +1,26 @@
+from cinderclient.v1 import client
+from credentials import get_cinder_credentials
+
+credentials = get_cinder_credentials()
+
+try:
+    cinder_client = client.Client(*credentials, service_type="volume")
+    vol_list = cinder_client.volumes.list()
+
+    vol_name = "voltest2"
+    vol_type = "test"
+    vol_size = 1
+    vol_exists = False
+    for v in vol_list:
+        if v == vol_name:
+            print "volume %s exists" % vol_name
+            vol_exists = True
+
+    if vol_exists == False:
+        volume = cinder_client.volumes.create(1, display_name=vol_name,
+                                             volume_type=vol_type)
+
+        print volume
+
+finally:
+    print "Execution completed"
