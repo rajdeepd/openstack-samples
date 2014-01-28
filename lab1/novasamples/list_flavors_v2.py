31e92a6b8ec8446094b336745864005fa5532371
Rajdeep Dua <dua_rajdeep@yahoo.com>
2014-01-28 16:33:16 +0530

    new commited: "list_flavors was wrong now corrected"

--- lab1/novasamples/list_flavors_v2.py
+++ lab1/novasamples/list_flavors_v2.py
@@ -4,4 +4,4 @@ from novaclient.client import Client
 credentials = get_nova_credentials_v2()
 nova_client = Client(**credentials)
 
-print  nova_client.images.list()
+print  nova_client.flavors.list()
