df0eab2fa556e00ebe16a303d8ee7d6442c2976b
bluespace9214 <bluespace9214@gmail.com>
2014-01-30 15:08:08 +0530

    "listing volume types","creating a volume type"...........checked with pep8

---
+++ lab1/cindersamples/list_types_volumes_v2.py
@@ -0,0 +1,12 @@
+import os
+from cinderclient.v2 import client
+from credentials import get_cinder_credentials
+
+credentials = get_cinder_credentials()
+
+try:
+    cinder_client = client.Client(*credentials, service_type="volume")
+    print cinder_client.volume_types.list()
+
+finally:
+    print "Execution completed"
